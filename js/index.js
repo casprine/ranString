9ce644c96c3017e7ddac42ee2734c7971699799f
casprine <casprine.001@gmail.com>
2017-12-07 17:45:40 +0000

    :fire: refactored code

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function randomString() {
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var string_length = 15;
+    var randomstring = '';
+    for (var i = 0; i < string_length; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring += chars.substring(rnum, rnum + 1);
+    }
+    document.order.randomfield.value = randomstring;
+}
+
+
+
+
+
+const btn = document.getElementById('fig');
+btn.addEventListener('click', randomString);
